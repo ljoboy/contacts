f80cdbc1f071c03c2487c65f22ce12f8656997e1
Jonathan Yombo <jonathanyombo@gmail.com>
2019-06-10 00:46:41 +0200

    [STASHED] every work perfectly

--- src/app/contacts-list/contacts-list.component.ts
+++ src/app/contacts-list/contacts-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Contact} from '../shared/contact';
 
 @Component({
   selector: 'app-contacts-list',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactsListComponent implements OnInit {
 
+
+  contacts: Array<Contact> = new Array<Contact>();
+
+
   constructor() { }
 
   ngOnInit() {
+    for (let i = 0; i < 100; i++) {
+      const c: Contact = new Contact(Math.floor(Math.random() * 9999999999));
+      c.id = i;
+      this.contacts.push(c);
+    }
   }
 
 }
