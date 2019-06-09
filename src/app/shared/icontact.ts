31a44177dd6264f15fd06d7d3383e1f057091df8
Jonathan Yombo <jonathanyombo@gmail.com>
2019-06-09 23:31:13 +0200

    [UPDATE] add new attributes

--- src/app/shared/icontact.ts
+++ src/app/shared/icontact.ts
@@ -1,3 +1,12 @@
-export interface IContact {
+import {Phone} from './phone';
 
+export interface IContact {
+  nom: string;
+  postnom: string;
+  prenom: string;
+  cree_le: Date;
+  etat: boolean;
+  genre: string;
+  img_url: string;
+  telephones: Phone[];
 }
