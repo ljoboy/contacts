c0f33401bd48e8b9f4e2369c1af57da2207dff1e
Jonathan Yombo <jonathanyombo@gmail.com>
2019-06-16 21:35:45 +0200

    [UPDATE] saveContact method

--- src/app/add-contact/add-contact.component.ts
+++ src/app/add-contact/add-contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Contact} from '../shared/contact';
 
 @Component({
   selector: 'app-add-contact',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddContactComponent implements OnInit {
 
+  contact: Contact = new Contact();
   constructor() { }
 
   ngOnInit() {
+  }
+
+  saveContact() {
+    console.log(this.contact);
   }
 
 }
